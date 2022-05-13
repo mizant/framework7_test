fbaff0a9d72acf1a5179310d903784f42956406c
Mizantrop <mizzantrop@yahoo.com>
2022-05-13 13:03:01 +0300

    доделана главная страничка

--- my-app.js
+++ my-app.js
@@ -1,27 +1,38 @@
 var app = new Framework7({
-    // App root element
-    el: '#app',
-    // App Name
-    name: 'My App',
-    // App id
-    id: 'com.myapp.test',
-    // Enable swipe panel
-    panel: {
-      swipe: false,
+  // App root element
+  el: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipeOnlyClose: true,
+
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/about/',
+      url: 'about.html',
     },
-    // Add default routes
-    routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
-    ],
-    // ... other parameters
-  });
+  ],
+  // ... other parameters
+});
 
-  function replaceContentInContainer(id, href, width = '100%', height = null) {
 
-    document.getElementById(id).innerHTML = '<iframe width="' + '100%' + '" height="' + height + '"  src="' + href +
-        '?autoplay=1&amp;loop=1&amp;" frameborder="0" allowfullscreen></iframe>'
+
+var gauge = app.gauge.get('.my-gauge');
+
+gauge.update({
+  size: 100,
+  borderWidth: 5,
+});
+
+
+function replaceContentInContainer(id, href, width = '100%', height = null) {
+
+  document.getElementById(id).innerHTML = '<iframe width="' + '100%' + '" height="' + height + '"  src="' + href +
+      '?autoplay=1&amp;loop=1&amp;" frameborder="0" allowfullscreen></iframe>'
 
 }
